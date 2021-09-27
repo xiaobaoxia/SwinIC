85c458f5a5f223e29700ccc8cc8eaeb4187c1736
小爆虾 <33535066+xiaobaoxia@users.noreply.github.com>
2021-09-27 23:45:00 +0800

    Update train.py

--- train.py
+++ train.py
@@ -134,7 +134,8 @@ def test(net,epoch,val_data,val_loader,val_transform,criterion):
             v_psnr = torch.mean(20 * torch.log10(255 / torch.sqrt(v_mse)), 0)
             bpp_y = latent_rate
             bpp_z = hyperlatent_rate
-	    list_test_v_loss += v_loss.item()
+            list_test_v_loss +=v_loss.item()
+            list_test_v_mse += v_mse.item()
             list_test_v_mse += v_mse.item()
             list_test_v_psnr += v_psnr.item()
             list_test_v_ms_ssim += v_ms_ssim.item()
